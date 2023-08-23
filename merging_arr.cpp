aa8b819f07bcfdff90a854fe9f0429f42c985208
Namit <namityadav008@gmail.com>
2023-08-23 16:35:27 +0530

    updates

--- merging_arr.cpp
+++ merging_arr.cpp
@@ -1,27 +1,16 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
 
+class MyClass {
+public:
+    inline void displayMessage() {
+        cout << "Hello, World!" << endl;
+    }
+};
+
 int main() {
-    int size;
-    cin >> size;
-    int elements[size];
-    const int MAX_ELEMENTS = 1000;
-    int frequency[MAX_ELEMENTS] = {0};
-
-    for(int i = 0; i < size; i++) {
-        cin >> elements[i];
-        frequency[elements[i]]++;
-    }
-
-    cout << "Element Frequency" << endl;
-
-    for(int i = 0; i < size; i++) {
-        if(frequency[elements[i]] > 0) {
-            cout << left << setw(10) << elements[i] << setw(10) << frequency[elements[i]] << endl;
-            frequency[elements[i]] = 0; 
-        }
-    }
+    MyClass obj;
+    obj.displayMessage();
 
     return 0;
 }
