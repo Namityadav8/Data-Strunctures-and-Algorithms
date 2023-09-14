e36acd7553f1d33c0426af6874943f150c209bd9
Namit <namityadav008@gmail.com>
2023-09-14 23:39:08 +0530

    updates

---
+++ class_DSA/practise .cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std ;
+class namit{
+public:
+    namit(){
+        cout<<"hi";
+    }
+
+    ~namit(){
+        cout<<"bye";
+        
+    }
+    
+};
+   
+int main(){
+    namit yadav;
+    
+    return 0;
+}
